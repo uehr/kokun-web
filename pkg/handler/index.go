9ed64a7842e9946a012712dfb29a00caa5a58b05
uehr <uehara.yudai@gmail.com>
2019-06-23 23:22:10 +0900

    add: senryu image drawer

--- pkg/handler/index.go
+++ pkg/handler/index.go
@@ -2,19 +2,70 @@ package handler
 
 import (
 	"net/http"
+	"net/url"
 	"text/template"
-	"time"
+
+	"github.com/uehr/kokun/pkg/imageProcess"
+	"github.com/uehr/kokun/pkg/senryu"
 )
 
 const tplPath = "views/index.tpl"
+
+const FirstSentenceQueryKey = "first"
+const SecondSentenceQueryKey = "second"
+const ThirdSentenceQueryKey = "third"
+const AuthorNameQueryKey = "author"
+
+func hasRequiredQuery(query url.Values) bool {
+	_, isExists := query[FirstSentenceQueryKey]
+	if !isExists {
+		return false
+	}
+
+	_, isExists = query[SecondSentenceQueryKey]
+	if !isExists {
+		return false
+	}
+
+	_, isExists = query[ThirdSentenceQueryKey]
+	if !isExists {
+		return false
+	}
+
+	_, isExists = query[AuthorNameQueryKey]
+	if !isExists {
+		return false
+	}
+
+	return true
+}
 
 func Index(w http.ResponseWriter, req *http.Request) {
 	// テンプレートをパース
 	tpl := template.Must(template.ParseFiles(tplPath))
 
-	m := map[string]string{
-		"Date": time.Now().Format("2006-01-02"),
-		"Time": time.Now().Format("15:04:05"),
+	// クエリを読み込み川柳を作成
+	query := req.URL.Query()
+	m := map[string]string{}
+
+	if hasRequiredQuery(query) {
+		sen := senryu.Senryu{
+			FirstSentence:  query.Get(FirstSentenceQueryKey),
+			SecondSentence: query.Get(SecondSentenceQueryKey),
+			ThirdSentence:  query.Get(ThirdSentenceQueryKey),
+			AuthorName:     query.Get(AuthorNameQueryKey),
+		}
+
+		option := senryu.SenryuImageOption{}
+		senryuImage, err := senryu.CreateImage(&sen, &option)
+		if err != nil {
+			return
+		}
+
+		base64Image := imageProcess.ImageBase64Encode(senryuImage)
+		senryuImageHTML := "<img src='data:image/png;base64," + base64Image + "' />"
+
+		m = map[string]string{"senryuImage": senryuImageHTML}
 	}
 
 	// テンプレートを描画
