f311587f39f373d3bb5da9a39200f5db9db9b187
uehr <uehara.yudai@gmail.com>
2019-06-24 14:09:21 +0900

    add: get senryu image func

---
+++ scripts/senryu.js
@@ -0,0 +1,28 @@
+const apiEndpoint = "http://localhost:12345/senryu"
+
+function getSenryuImage(firstSentence, secondSentence, thirdSentence, authorName) {
+    const postSenryu = {
+        "FirstSentence": firstSentence,
+        "SecondSentence": secondSentence,
+        "ThirdSentence": thirdSentence,
+        "AuthorName": authorName
+    }
+
+    return postData(apiEndpoint, postSenryu)
+}
+
+function postData(url = ``, data = {}) {
+    return fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json());
+}
