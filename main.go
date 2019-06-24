ef19e985c16b90c87b8b5d0d17c1bc20b19435eb
uehr <uehara.yudai@gmail.com>
2019-06-24 22:46:34 +0900

    add: media dir

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts/"))))
+	http.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir("media/"))))
 
 	http.ListenAndServe(":"+port, nil)
 }
