20fd91dde2096de01f0892b5a1f460796a4aebe0
uehr <uehara.yudai@gmail.com>
2019-06-25 23:59:27 +0900

    update: navigation{nb} -> nextButton

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
     new Swiper('.swiper-container', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
         loop: true,
         autoplay: true,
         slidesPerView: 3,
         spaceBetween: 10,
         centeredSlides: true,
         disableOnInteraction: false,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
         breakpoints: {
             767: {
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                loop: true,
+                autoplay: true,
                 slidesPerView: 1,
                 spaceBetween: 0,
                 centeredSlides: true,
-                loop: true,
-                autoplay: true,
+                disableOnInteraction: false,
             }
         }
     });
