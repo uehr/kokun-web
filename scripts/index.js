cb296665978e61e899a3918f0d2219f0a0675491
uehr <uehara.yudai@gmail.com>
2019-06-26 00:01:14 +0900

    add: showResultModal

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $("#download-button").click(() => {
+    })
+
     $("#senryu-create-button").click(() => {
         const first = $("#first-sentence").val()
         const second = $("#second-sentence").val()
@@ -7,10 +10,17 @@ $(document).ready(function () {
 
         getSenryuImage(first, second, third, author).then(res => {
             $("#senryu-image").attr("src", "data:image/png;base64," + res.base64Img)
+            $("#senryu-image-preview").attr("src", "data:image/png;base64," + res.base64Img)
+
             $("#first-sentence").val("")
             $("#second-sentence").val("")
             $("#third-sentence").val("")
             $("#author-name").val("")
+
+            showResultModal()
+        }).catch(err => {
+            alert("生成に失敗しました")
         })
     })
-})
+
+});
