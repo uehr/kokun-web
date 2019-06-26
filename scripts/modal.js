9f60325ce9725c78aab2aae25b1b3c65f42f98d4
uehr <uehara.yudai@gmail.com>
2019-06-26 10:39:37 +0900

    fix: modalが中央からずれるバグ

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,27 +1,26 @@
 function showResultModal() {
-    $("body").append('<div id="modal-bg"></div>');
-    modalResize();
-
-    $("#modal-bg,#modal-main").fadeIn("slow");
+    new Promise(resolve => {
+        resole($("body").append('<div id="modal-bg"></div>'));
+    }).then(res => {
+        $("#modal-bg,#modal-main").fadeIn("slow");
+    })
 
     $("#close-modal-button").click(() => {
         removeResultModal()
     })
+}
 
-    $(window).resize(modalResize);
+function modalResize() {
+    var w = $(window).width();
+    var h = $(window).height();
 
-    function modalResize() {
-        var w = $(window).width();
-        var h = $(window).height();
+    var cw = $("#modal-main").outerWidth();
+    var ch = $("#modal-main").outerHeight();
 
-        var cw = $("#modal-main").outerWidth();
-        var ch = $("#modal-main").outerHeight();
-
-        $("#modal-main").css({
-            "left": ((w - cw) / 2) + "px",
-            "top": ((h - ch) / 2) + "px"
-        });
-    }
+    $("#modal-main").css({
+        "left": ((w - cw) / 2) + "px",
+        "top": ((h - ch) / 2) + "px"
+    });
 }
 
 function removeResultModal() {
