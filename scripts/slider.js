107fcbe9925a5f7c1cf74c1571bdf847f2395ebc
uehr <uehara.yudai@gmail.com>
2019-06-26 14:40:48 +0900

    fix: next/prevボタンが効かないバグ

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
     new Swiper('.swiper-container', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
         loop: true,
         autoplay: true,
         slidesPerView: 3,
         spaceBetween: 10,
         centeredSlides: true,
         disableOnInteraction: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
         breakpoints: {
             767: {
-                nextButton: '.swiper-button-next',
-                prevButton: '.swiper-button-prev',
-                loop: true,
-                autoplay: true,
                 slidesPerView: 1,
                 spaceBetween: 0,
-                centeredSlides: true,
-                disableOnInteraction: false,
             }
         }
     });
